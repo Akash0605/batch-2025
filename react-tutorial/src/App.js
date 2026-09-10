93a32b13a3cb307449ff4676487ef6ebdc6eb800
Vikas <vikas.1@mediatechtemple.com>
2026-09-10 18:51:45 +0530

    Counter program.

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./component/navbar/Navbar"
 import Footer from "./component/footer/Footer"
 import Login from "./component/login/Login"
 
+import { useState } from 'react';
+
 function App() {
 
   const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -13,13 +15,40 @@ function App() {
   const isLoggedInBool = (isLoggedIn === "true");
   console.log("Type of isLoggedInBool : ", typeof isLoggedInBool);
 
+  const [count, setCount] = useState(0);
+
+
+  const increment = () => {
+    console.log("Increment button clicked");
+    // count++;
+
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    console.log("Decrement button clicked");
+    // count--;
+
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <Navbar isLoggedIn={isLoggedInBool} />
 
-      {
+      {/* {
         isLoggedInBool ? <UserList /> : <Login />
-      }
+      } */}
+
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px" }}>
+
+        <h1>Current Count: {count}</h1>
+
+        <div style={{ display: "flex", gap: "10px" }}>
+          <button onClick={increment}>+ Increment</button>
+          <button onClick={decrement}>- Decrement</button>
+        </div>
+      </div>
 
       <Footer />
     </div>
